26cee76bc7ba33bdd0d7f5260c9368e91676f7a6
African Jedi <fk.posh697@passinbox.com>
2025-10-21 15:30:14 +0200

    Update order controller

--- .NetCore9/WebApi/RabbitMQ.ProducerAndConsumer.WebApi/Controllers/OrderController.cs
+++ .NetCore9/WebApi/RabbitMQ.ProducerAndConsumer.WebApi/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using RabbitMQ.ProducerAndConsumer.WebApi.Model;
 
@@ -8,14 +9,29 @@ namespace RabbitMQ.ProducerAndConsumer.WebApi.Controllers;
 [Route("api/[controller]")]
 public class OrderController : ControllerBase
 {
-   [HttpGet]
-   public IActionResult GetOrders()
-   {
-       // Logic to retrieve orders
-       return Ok(new OrderDTO[]
-       {
+    [HttpGet]
+    public async Task<OrderDTO[]> GetOrders()
+    {
+        // Logic to retrieve orders
+        return
+        [
            new OrderDTO { Id = 1, ProductName = "Product A", Quantity = 2 },
            new OrderDTO { Id = 2, ProductName = "Product B", Quantity = 5 }
-       });
-   }
+        ];
+    }
+
+    [HttpPost]
+    public async Task<Results<CreatedAtRoute, BadRequest<string>, ProblemHttpResult>> CreateOrder([FromBody] OrderDTO order)
+    {
+        if (order == null || string.IsNullOrEmpty(order.ProductName) || order.Quantity <= 0)
+        {
+            return TypedResults.BadRequest("Invalid order data.");
+        }
+
+        // Logic to create the order
+        // For demonstration, we'll just return the created order with a dummy ID
+        order.Id = new Random().Next(1000, 9999);
+
+        return TypedResults.CreatedAtRoute("GetOrderById", new { id = order.Id }, order);
+    }
 }
